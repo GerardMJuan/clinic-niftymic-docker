cd3d7d28d6f98c7ba2479dacf145385f49f96ab1
Gerard Martí <gerardmartijuan@gmail.com>
2025-01-29 15:56:45 +0100

    refactor: update run_docker_niftymic.bat to include DICOM_DIR and adjust directory structure; add instruction manuals in Catalan and Spanish

--- scripts_to_run/run_docker_niftymic.bat
+++ scripts_to_run/run_docker_niftymic.bat
@@ -2,18 +2,26 @@
 setlocal enabledelayedexpansion
 
 :: Hardcoded directories
-set "INPUT_DIR=input"
+set "INPUT_DIR=nifti"
 set "OUTPUT_DIR=output"
+set "DICOM_DIR=dicom"
 
 :: Get absolute paths
 for %%i in ("%INPUT_DIR%") do set "INPUT_DIR=%%~fi"
 for %%i in ("%OUTPUT_DIR%") do set "OUTPUT_DIR=%%~fi"
+for %%i in ("%DICOM_DIR%") do set "DICOM_DIR=%%~fi"
 
-:: Check if input directory exists
-if not exist "%INPUT_DIR%\" (
-    echo Error: Input directory %INPUT_DIR% does not exist
+:: Check if DICOM directory exists
+if not exist "%DICOM_DIR%\" (
+    echo Error: DICOM input directory %DICOM_DIR% does not exist
     pause
     exit /b 1
+)
+
+:: Create input directory if it doesn't exist
+if not exist "%INPUT_DIR%\" (
+    echo Creating input directory %INPUT_DIR%
+    mkdir "%INPUT_DIR%"
 )
 
 :: Create output directory if it doesn't exist
@@ -22,12 +30,14 @@ if not exist "%OUTPUT_DIR%\" (
     mkdir "%OUTPUT_DIR%"
 )
 
+
 :: Run the docker container with NVIDIA GPU support
 docker run ^
     --rm ^
     --ipc=host ^
-    -v "%INPUT_DIR%":/app/data/inputs ^
+    -v "%INPUT_DIR%":/app/data/nifti ^
     -v "%OUTPUT_DIR%":/app/data/outputs ^
+    -v "%DICOM_DIR%":/app/data/dicom ^
     gerardmartijuan/docker-niftymic-clinic:latest
 
 :: Check if docker run was successful
